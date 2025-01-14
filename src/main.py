e758f93da81c248944e88919ecdf0d1e68b89125
Andrej <andrej@mail.com>
2025-01-15 00:01:18 +0100

    code is ready

---
+++ src/main.py
@@ -0,0 +1,63 @@
+import json
+import os
+
+
+class Product:
+    def __init__(self, name: str, description: str, price: float, quantity: int):
+        self.name = name
+        self.description = description
+        self.price = price
+        self.quantity = quantity
+
+
+class Category:
+    category_count = 0
+    total_products_count = 0
+
+    def __init__(self, name: str, description: str):
+        self.name = name
+        self.description = description
+        self.products = []
+        Category.category_count += 1
+
+    def add_product(self, product: Product):
+        self.products.append(product)
+        Category.total_products_count += 1
+
+
+### Дополнительное задание
+
+
+def get_information_from_json(filepath: str):
+    categories = []
+    try:
+        if not os.path.exists(filepath):
+            raise FileNotFoundError(f"Файл {filepath} не найден.")
+        with open(filepath, "r", encoding="utf-8") as file:
+            data = json.load(file)
+            for category_data in data:
+                category = Category(name=category_data["name"], description=category_data["description"])
+                for product_data in category_data.get("products", []):
+                    product = Product(
+                        name=product_data["name"],
+                        description=product_data["description"],
+                        price=product_data["price"],
+                        quantity=product_data["quantity"],
+                    )
+                    category.add_product(product)
+                categories.append(category)
+    except (FileNotFoundError, json.JSONDecodeError) as e:
+        print(f"Ошибка загрузки данных из файла: {e}")
+    return categories
+
+
+# if __name__ == "__main__":
+#     filepath = "/home/andrej/Poetry_homework/OOP/data/products.json"
+#     categories = get_information_from_json(filepath)
+#
+#     for category in categories:
+#         print(f"\nКатегория: {category.name}")
+#         print(f"Описание: {category.description}")
+#         print(f"Количество товаров: {len(category.products)}")
+#     for product in category.products:
+#         print(f"  - {product.name}: {product.price} руб., {product.quantity} шт.")
