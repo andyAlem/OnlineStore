c81934293dd1726dc8dedc1fca704b27f23c84dd
Andrej <andrej@mail.com>
2025-01-20 19:22:31 +0100

    code is done, exs 2,3,4

--- src/main.py
+++ src/main.py
@@ -6,8 +6,36 @@ class Product:
     def __init__(self, name: str, description: str, price: float, quantity: int):
         self.name = name
         self.description = description
-        self.price = price
+        self.__price = price
         self.quantity = quantity
+
+    @classmethod
+    def new_product(cls, product_data: dict):
+        """Класс-метод принимает на вход параметры товара в словаре и возвращать созданный объект класса """
+        required_keys = ["name", "description", "price", "quantity"]
+        for k in required_keys:
+            if k not in product_data:
+                raise ValueError(f"Отсутствует обязательное поле: {k}")
+
+        return cls(
+            name=product_data["name"],
+            description=product_data["description"],
+            price=product_data["price"],
+            quantity=product_data["quantity"],
+        )
+
+    @property
+    def price(self):
+        """Геттер для получения значения цены."""
+        return self.__price
+
+    @price.setter
+    def price(self, new_price: float):
+        """Сеттер устанавливает новую цену"""
+        if new_price <= 0:
+            print("Цена не должна быть нулевая или отрицательная")
+        else:
+            self.__price = new_price
 
 
 class Category:
@@ -27,12 +55,25 @@ class Category:
         self.__products.append(product)
         Category.total_products_count += 1
 
+    @property
+    def product(self):
+        """Геттер выводит список товаров в формате строки."""
+        if not self.__products:
+            return "В категории нет товаров."
+        return "\n".join(
+            [
+                f"{product.name}, {product.price} руб. Остаток: {product.quantity} шт."
+                for product in self.__products
+            ]
+        )
+
+
 
 ### Дополнительное задание
 
 
 def get_information_from_json(filepath: str):
-    """ "Функция для загрузки данных по категориям и товарам из файла JSON"""
+    """ Функция для загрузки данных по категориям и товарам из файла JSON"""
     categories = []
     try:
         if not os.path.exists(filepath):
@@ -65,6 +106,17 @@ def get_information_from_json(filepath: str):
 #     for category in categories:
 #         print(f"\nКатегория: {category.name}")
 #         print(f"Описание: {category.description}")
-#         print(f"Количество товаров: {len(category.products)}")
-#     for product in category.products:
-#         print(f"  - {product.name}: {product.price} руб., {product.quantity} шт.")
+#         print("Список товаров:")
+#         print(category.product)
+#
+#     if categories and categories[0].product:
+#
+#         first_product = categories[0]._Category__products[0]
+#         print(f"\nТекущая цена первого товара: {first_product.price} руб.")
+#
+#         first_product.price = 200
+#         print(f"Обновленная цена первого товара: {first_product.price} руб.")
+#
+#         first_product.price = -50
+#         print(f"Цена после изменения(Должна остаться такой же!) : {first_product.price} руб.")
+
